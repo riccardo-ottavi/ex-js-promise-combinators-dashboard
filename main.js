d7d9e82f0953356ffc6708e2fcfeadd42933a1c4
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-18 11:36:48 +0100

    testa fetch

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-console.log("Prova")
+console.log("Provaaaa")
 
+async function getDashboardData(query){
+    const response = await fetch(`http://localhost:3333/destinations?search=${query}`)
+    const destination = await response.json();
+    console.log(destination)
+}
+
+
+getDashboardData('london')
+    .then(data => {
+        console.log('Dasboard data:', data);
+        console.log(
+            `${data.city} is in ${data.country}.\n` +
+            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
+            `The main airport is ${data.airport}.\n`
+        );
+    })
+    .catch(error => console.error(error));
