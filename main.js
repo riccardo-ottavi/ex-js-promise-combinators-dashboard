86632adf3e6e71460c0524d4466b3b7de6c39b14
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-18 17:53:00 +0100

    inizializza promises

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 console.log("Provaaaa")
 
+async function fetchJson(url){
+    const response = await fetch(url);
+    const obj = await response.json();
+    return obj
+}
+
 async function getDashboardData(query){
     const promises = []
-    const destinationPromise = fetch(`http://localhost:3333/destinations?search=${query}`)
-    const weatherPromise = fetch(`http://localhost:3333/weathers?search=${query}`)
-    const airportPromise = fetch(`http://localhost:3333/airports?search=${query}`)
-
+    const destination = fetchJson(`http://localhost:3333/destinations?search=${query}`)
+    const weather = fetchJson(`http://localhost:3333/weathers?search=${query}`)
+    const airport = fetchJson(`http://localhost:3333/airports?search=${query}`)
+    promises.push(destination, weather, airport)
+    const data = await Promise.all(promises)
+    return data
 }
 
 
